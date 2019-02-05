449125db4ef3c59a3811829552237290933a78fa
Farrukh A. Khan <farukhcs15@gmail.com>
2019-02-05 15:12:29 +0100

    Update newDataLabeling.py

--- newDataLabeling.py
+++ newDataLabeling.py
@@ -54,39 +54,3 @@ print(len(training_labels))
 
 np.save('D:/Inception_preprocessed_data_Labels_2004/Morethan1000samplesData/Training_Data_1000Samples',training_data)
 np.save('D:/Inception_preprocessed_data_Labels_2004/Morethan1000samplesData/Training_Labels_1000Samples',training_labels)
-# print("Successfully Saved...! Training Data And Labels")
-
-# del training_data, training_labels
-
-# print("Building Testing Data Now ...")
-
-# for y in test:
-
-#   abspath_test = str(path + y).rstrip("\n\r")
-#   print(abspath_test)
-  
-#   read_image_test = mpg.imread(abspath_test)
-#   read_image_test_resized = cv2.cv2.resize(read_image_test,(224,224))
-#   read_image_float16_test = read_image_test_resized.astype('float32')
-#   read_image_test_resized_preprocess = read_image_float16_test / 255.0
-
-#   testing_data.append(read_image_test_resized_preprocess)
-#   testing_labels.append(y.rsplit('/')[0])
-#   # print(test_labels)
-#   # print(testing_labels)
-
-# test.close()
-# import gc
-# gc.collect()
-
-# np.save('D:/Inception_preprocessed_data_Labels_2004/Top5/TestingData_Top5',testing_data)
-# np.save('D:/Inception_preprocessed_data_Labels_2004/Top5/TestingLabels_Top5',testing_labels)
-
-# plt.imshow(testing_data[1900])
-# plt.show()
-
-# print(len(testing_labels))
-# print("Successfully saved...! Testing Data and Labels")
-
-# print("Aborting.... Enjoy Machine Learning You A**HOLE")
-# del testing_data, testing_labels
