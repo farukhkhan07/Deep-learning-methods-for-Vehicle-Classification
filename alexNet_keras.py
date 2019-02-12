abd2de1821a8657f9a6d5e7bdaa6fd2f18aec401
farukhkhan07 <farukhcs15@gmail.com>
2019-02-12 11:52:31 +0100

    AlexNetArchitecture

--- alexNet_keras.py
+++ alexNet_keras.py
@@ -9,18 +9,20 @@ from keras.utils import np_utils
 from random import shuffle
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import matplotlib.image as mpg
+import cv2
 np.random.seed(1000)
 
 # (2) Get Data
 # import tflearn.datasets.oxflower17 as oxflower17
 # x, y = oxflower17.load_data(one_hot=True)
 
-X_train = np.load('D:/Inception_preprocessed_data_Labels_2004/Morethan2000samplesData/Training_Data_2000Samples_chunk0.npy')#('D:/ThesisWork/S_224_Training_data.npy')#training_images
-X_test = np.load('D:/Inception_preprocessed_data_Labels_2004/Morethan2000samplesData/Training_labels_2000Samples_chunk0.npy')#('D:/ThesisWork/S_224_Training_labels.npy')#training_labels
+X_train = np.load('D:/Inception_preprocessed_data_Labels_2004/Morethan4000samplesData/250250/training_images.npy')#('D:/ThesisWork/S_224_Training_data.npy')#training_images
+X_test = np.load('D:/Inception_preprocessed_data_Labels_2004/Morethan4000samplesData/250250/trainin_labels.npy')#('D:/ThesisWork/S_224_Training_labels.npy')#training_labels
 print(X_test)
-exit()
-plt.imshow(X_train[1500])
-plt.show()
+# exit()
+# plt.imshow(X_train[1500])
+# plt.show()
 # X_train = X_train/255.0
 le = preprocessing.LabelEncoder()
 le.fit(X_test)
@@ -35,7 +37,7 @@ shuffle(train_labels_hotEncode)
 model = Sequential()
 
 # 1st Convolutional Layer
-model.add(Conv2D(filters=96, input_shape=(224,224,3), kernel_size=(11,11), strides=(4,4), padding='valid'))
+model.add(Conv2D(filters=96, input_shape=(250,250,3), kernel_size=(11,11), strides=(4,4), padding='valid'))
 model.add(Activation('relu'))
 # Pooling 
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
@@ -73,7 +75,7 @@ model.add(BatchNormalization())
 # Passing it to a dense layer
 model.add(Flatten())
 # 1st Dense Layer
-model.add(Dense(4096, input_shape=(224*224*3,)))
+model.add(Dense(4096, input_shape=(250*250*3,)))
 model.add(Activation('relu'))
 # Add Dropout to prevent overfitting
 model.add(Dropout(0.4))
@@ -97,20 +99,20 @@ model.add(Dropout(0.4))
 model.add(BatchNormalization())
 
 # Output Layer
-model.add(Dense(23))
+model.add(Dense(5))
 model.add(Activation('softmax'))
 
 model.summary()
 # ndm = keras.optimizers.nadam(lr = 0.0001, epsilon=1e-08)
 # keras.optimizers.nadam()
 # (4) Compile 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 # (5) Train
-model.fit(X_train, train_labels_hotEncode, batch_size=64, epochs=10, verbose=1,validation_split=0.3)# shuffle=True
+model.fit(X_train, train_labels_hotEncode, batch_size=32, epochs=50, verbose=1,validation_split=0.3, shuffle=True)# shuffle=True
 
-predict = X_train
-
+predict = mpg.imread("D:/Morethan4000samples/testdata/39/1367/2007/1e39f2a877b507.jpg")
+predict_resize = cv2.cv2.resize(predict,(250,250))
 mod = model.predict(predict)
 print(mod.shape)
 print(mod)
